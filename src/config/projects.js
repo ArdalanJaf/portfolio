57a9eaf65d0ead02e518f8f11a55e9496ce2b69b
Ardo Al-Jaf <a.aljaf@gmail.com>
2022-05-27 14:28:13 +0100

    project image style fix

--- src/config/projects.js
+++ src/config/projects.js
@@ -7,7 +7,7 @@ export const projects = [
       gitBack: "https://github.com/ArdalanJaf/find-sparks-back",
     },
     notes:
-      "A dating app created to showcase front and back-end abilities. Features include multi-user log-in authentification and a unique points-based matching algorythm.",
+      "A dating app created to showcase front and back-end abilities. Features include multi-user log-in middleware authentification and a unique points-based matching algorythm.",
     image: "findsparks.png",
     tech: "React - Redux | Node.js - Express | SQL | Bootstrap",
   },
@@ -18,7 +18,7 @@ export const projects = [
       site: "https://ft1-hackathon.netlify.app/",
     },
     notes:
-      "This app was made in a team of four as part of a hackathon challenge. The goal was to produce an app in 6 hours to help children learning to read.",
+      "This app was made in a team of four as part of a hackathon challenge. The goal was to produce an app in 6 hours to help children learning to read. Through using a text-to-speech API, users are encouraged to practice pronunciation.",
     image: "readeasy.png",
     tech: "React | Axios | JS | SASS | GSAP ",
   },
@@ -29,9 +29,9 @@ export const projects = [
       site: "https://ardalan-weather-app.netlify.app/",
     },
     notes:
-      "Weather app that showcases ability to utilise APIs using axios. It uses openMapGL and open-weather-map to provide an 8-day forecast for anywhere in the world.",
+      "Fully-responsive weather app that demonstrates my ability to utilise APIs using axios. It uses openMapGL and open-weather-map to provide an 8-day forecast for anywhere in the world.",
     image: "weatherApp.png",
-    tech: "HTML | AXIOS | JS | SASS",
+    tech: "HTML | Axios | JS | SASS",
   },
   {
     name: "Nowness Homage",
