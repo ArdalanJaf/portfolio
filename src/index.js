b55742c1cdcd0ed1f1cf3a17b04dc3ee05a6168b
Ardalan Al-Jaf <ardalan@switch2zero.com>
2024-10-19 19:03:23 +0100

    git hub pages - brwoserRouter

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { reducer } from "./redux/reducer";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
 const store = createStore(
   reducer,
@@ -16,7 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
